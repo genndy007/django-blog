d1b278cf6be5ea8066035a46307fa9979a8f0cf1
genndy007 <kochevgenazp@gmail.com>
2021-08-20 14:27:58 +0300

    Add crud and tests

--- blog/tests.py
+++ blog/tests.py
@@ -8,6 +8,8 @@ from .models import Post
 
 extitle = 'title'
 exbody = 'body'
+extitle2 = 'new title'
+exbody2 = 'new body'
 exusername = 'test'
 
 post_url = '/post'
@@ -44,17 +46,37 @@ class BlogTests(TestCase):
         self.assertTemplateUsed(res, 'home.html')
 
     def test_post_detail_view(self):
-        res = self.client.get(f'/post/1')
-        nores = self.client.get(f'{post_url}/{bad_index}')
+        res = self.client.get(f'{post_url}/1', follow=True)
+        nores = self.client.get(f'{post_url}/{bad_index}', follow=True)
 
-        self.assertEqual(res.status_code, 301)
+        self.assertEqual(res.status_code, 200)
         self.assertEqual(nores.status_code, 404)
         self.assertContains(res, extitle)
         self.assertTemplateUsed(res, 'post_detail.html')
 
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), f'{post_url}/1/')
 
+    def test_post_create_view(self):
+        res = self.client.post(reverse('post_new'), {
+            'title': extitle2,
+            'body': exbody2,
+            'author': self.user.pk,
+        }, follow=True)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(Post.objects.last().title, extitle2)
+        self.assertEqual(Post.objects.last().body, exbody2)
 
+    def test_post_update_view(self):
+        res = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        }, follow=True)
+        self.assertEqual(res.status_code, 200)
 
+    def test_post_delete_view(self):
+        res = self.client.post(reverse('post_delete', args='1'), follow=True)
+        self.assertEqual(res.status_code, 200)
 
 
 
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import (
+    BlogListView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogUpdateView,
+    BlogDeleteView
+)
+
+my_base_url = 'post'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path(f'{my_base_url}/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path(f'{my_base_url}/new/', BlogCreateView.as_view(), name='post_new'),
+    path(f'{my_base_url}/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'),
+    path(f'{my_base_url}/<int:pk>/delete', BlogDeleteView.as_view(), name='post_delete'),
 ]
